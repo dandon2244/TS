9e1b42a25c37a4e22d454968e057bf55a7e6daca
dandon2244 <dabebe3399@gmail.com>
2020-01-27 23:58:43 +0000

    a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("YOOO");
+let canvas = document.getElementById("gameCanvas");
+let context = canvas.getContext("2d");
+let cTime = 0;
+let lastTime = 0;
+let lastSec = 0;
+function update() {
+  cTime = performance.now();
+
+  //console.log(cTime);
+
+  context.fillStyle = "#fff8c7";
+  context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+  context.fillStyle = "blue";
+  context.fillRect(10, 10, 50, 50);
+}
+setInterval(update, 10);
