1098a9cd128417636e9b10cc648e6061664a32b8
dandon2244 <dabebe3399@gmail.com>
2020-01-28 01:35:53 +0000

    a

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
+let cameraPos = [0, 0];
 let cTime = 0;
 let lastTime = 0;
 let lastSec = 0;
-function update() {
+let dt = 0;
+let frames = 0;
+let position = 10;
+let direction = 0;
+function update(timestamp) {
+  frames++;
   cTime = performance.now();
+  dt = cTime - lastTime;
+  lastTime = cTime;
+  if (lastSec == 0) {
+    lastSec = cTime;
+  }
 
+  if (cTime - lastSec > 1000) {
+    console.log(frames);
+    lastSec = cTime;
+    frames = 0;
+  }
   //console.log(cTime);
-
+  if (!direction) {
+    if (position >= 500) {
+      direction = 1;
+    } else position += 40 / dt;
+  }
+  if (direction) {
+    if (position <= 50) {
+      direction = 0;
+    } else position -= 40 / dt;
+  }
   context.fillStyle = "#fff8c7";
   context.fillRect(0, 0, canvas.clientWidth, canvas.height);
   context.fillStyle = "blue";
-  context.fillRect(10, 10, 50, 50);
+  context.fillRect(position - cameraPos[0], 10 - cameraPos[1], 50, 50);
+  requestAnimationFrame(update);
 }
-setInterval(update, 10);
+update();
